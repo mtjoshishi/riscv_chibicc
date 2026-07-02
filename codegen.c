mtjoshishi/riscv_chibicc

commit 80fca30d5ba57a121a0bb53c80bf0b2d172ec97d

Add support multi-letter local variables
Signed-off-by: Makoto Teramoto <mteramoto.knct@gmail.com>

codegen.c
@@ -8,9 +8,9 @@
 /// @brief Generate local variable into the stack.
 static void gen_addr(struct Node *node) {
   CHECK(node != nullptr);
-  if (node->kind != NODE_LVAR)
+  if (node->kind != NODE_VAR)
     error("Not an lvalue.");
-  int offset = (node->name - 'a' + 1) * 8;
+  int offset = node->var->offset;
   printf("    addi t0, fp, -%d\n", offset);
   printf("    addi sp, sp, -8\n");
   printf("    sd t0, 0(sp)\n");
@@ -72,7 +72,7 @@ static void gen(struct Node *node) {
     printf("    addi sp, sp, -8\n");
     printf("    sd t0, 0(sp)\n");
     return;
-  case NODE_LVAR:
+  case NODE_VAR:
     gen_addr(node);
     load();
     return;
@@ -137,14 +137,15 @@ static void gen(struct Node *node) {
   printf("    sd t0, 0(sp)\n");
 }
 
-void codegen(struct Node *node) {
+void codegen(struct Program *prog) {
+  CHECK(prog != nullptr);
   printf(".globl main\n");
   printf("main:\n");
 
   prologue();
 
-  // Traverse the AST to emit assembly.
-  for (struct Node *n = node; n; n = n->next) {
+  // Emit the code
+  for (struct Node *n = prog->node; n; n = n->next) {
     gen(n);
     printf("    ld a0, 0(sp)\n");
     printf("    addi sp, sp, 8\n");

chibicc_types.h
@@ -33,8 +33,16 @@ enum NodeKind {
   NODE_LT,     // <
   NODE_LE,     // <=
   NODE_ASSIGN, // =
-  NODE_LVAR,   // Local variable
+  NODE_VAR,    // Variable
   NODE_NUM,    // number
+};
+
+// Variable
+struct Var {
+  struct Var *next; // Next variable
+  char *name;       // Name of variable
+  size_t len;       // Length of variable name
+  int offset;       // Offset of the stack from the frame pointer.
 };
 
 // Node for AST
@@ -43,8 +51,15 @@ struct Node {
   struct Node *next;  // Next node.
   struct Node *lhs;   // Left hand side statement
   struct Node *rhs;   // Right hand side statement
-  char name;          // Name of variable. Use if kind is NODE_LVAR.
+  struct Var *var;    // Object of variable. Use if kind is NODE_VAR.
   int val;            // Value if kind is NODE_NUM
+};
+
+// Program
+struct Program {
+  struct Node *node;
+  struct Var *locals;
+  int stack_size;
 };
 
 #endif // CHIBICC_TYPES_H_

codegen.h
@@ -3,6 +3,6 @@
 
 #include "chibicc_types.h"
 
-void codegen(struct Node *node);
+void codegen(struct Program *program);
 
 #endif // CODEGEN_H_

parse.c
@@ -1,10 +1,22 @@
 #include "parse.h"
 
 #include <stdlib.h>
+#include <string.h>
 
 #include "chibicc_error.h"
 #include "chibicc_types.h"
 #include "tokenize.h"
+
+struct Var *locals;
+
+struct Var *find_var(struct Token *token) {
+  CHECK(token != nullptr);
+  for (struct Var *var = locals; var; var = var->next)
+    if (strlen(var->name) == token->len &&
+        !memcmp(token->str, var->name, var->len))
+      return var;
+  return nullptr;
+}
 
 /**
  * @brief Create new node instance.
@@ -46,14 +58,25 @@ struct Node *new_num(int value) {
 }
 
 /**
- * @brief Create new local variable node.
+ * @brief Create new variable node.
  * @param name The name of local variable.
  * @return New 'NODE_LVAR' node.
  */
-struct Node *new_lvar(char name) {
-  struct Node *node = new_node(NODE_LVAR);
-  node->name = name;
+struct Node *new_var(struct Var *var) {
+  CHECK(var != nullptr);
+  struct Node *node = new_node(NODE_VAR);
+  node->var = var;
   return node;
+}
+
+struct Var *push_var(char *name) {
+  struct Var *var = calloc(1, sizeof(*var));
+  CHECK(var != nullptr);
+
+  var->next = locals;
+  var->name = name;
+  locals = var;
+  return var;
 }
 
 static struct Node *stmt(struct Token **token);
@@ -71,8 +94,10 @@ static struct Node *primary(struct Token **token);
  * @param token Tokenized source codes.
  * @return Node of `program`.
  */
-struct Node *program(struct Token **token) {
+struct Program *program(struct Token **token) {
   CHECK(token != nullptr && *token != nullptr);
+  locals = nullptr;
+
   struct Node head = {};
   head.next = nullptr;
   struct Node *cur = &head;
@@ -82,7 +107,11 @@ struct Node *program(struct Token **token) {
     CHECK(cur->next != nullptr);
     cur = cur->next;
   }
-  return head.next;
+
+  struct Program *prog = calloc(1, sizeof(*prog));
+  prog->node = head.next;
+  prog->locals = locals;
+  return prog;
 }
 
 /**
@@ -224,8 +253,12 @@ static struct Node *primary(struct Token **token) {
   }
 
   struct Token *tok = consume_ident(token);
-  if (tok != nullptr)
-    return new_lvar(*tok->str);
+  if (tok != nullptr) {
+    struct Var *var = find_var(tok);
+    if (var == nullptr)
+      var = push_var(strndup(tok->str, tok->len));
+    return new_var(var);
+  }
 
   return new_num(seek_if_expect_number(token));
 }

parse.h
@@ -3,6 +3,6 @@
 
 #include "chibicc_types.h"
 
-struct Node *program(struct Token **token);
+struct Program *program(struct Token **token);
 
 #endif // PARSE_H_

riscv_chibicc.c
@@ -9,9 +9,18 @@ int main(int argc, char **argv) {
     error("Invalid number of arguments.");
 
   struct Token *token = tokenize(argv[1]);
-  struct Node *node = program(&token);
-  CHECK(node != nullptr);
-  codegen(node);
+  struct Program *prog = program(&token);
+  CHECK(prog != nullptr);
+
+  // Assign offsets to local variables.
+  int offset = 0;
+  for (struct Var *var = prog->locals; var; var = var->next) {
+    offset += 8;
+    var->offset = offset;
+  }
+  prog->stack_size = offset;
+
+  codegen(prog);
 
   return 0;
 }

test.sh
@@ -54,5 +54,7 @@ assert 0 "1 >= 2;"
 assert 3 "1; 2; 3;"
 
 assert 3 "a=3; a;"
+assert 3 "foo=3; foo;"
+assert 8 "foo123=3; bar=5; foo123+bar;"
 
 echo OK

tokenize.c
@@ -131,6 +131,12 @@ static bool startswith(const char *p, const char *q) {
   return memcmp(p, q, strlen(q)) == 0;
 }
 
+static bool is_alpha(char c) {
+  return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
+}
+
+static bool is_alnum(char c) { return is_alpha(c) || ('0' <= c && c <= '9'); }
+
 struct Token *tokenize(char *input) {
   CHECK(input != nullptr);
   char *p = input;
@@ -161,9 +167,12 @@ struct Token *tokenize(char *input) {
     }
 
     // Identifier
-    if ('a' <= *p && *p <= 'z') {
-      cur = new_token(TK_IDENT, &cur, p, 1);
+    if (is_alpha(*p)) {
+      char *q = p;
       p++;
+      while (is_alnum(*p))
+        p++;
+      cur = new_token(TK_IDENT, &cur, q, (size_t)(p - q));
       cur->source_input = input;
       continue;
     }

commit 97989a8de2fc6b6aef4959da76086f872bd4947c

codegen: Apply stack size of progtam to prologue/epilogue
I missed applying the stack size of program. Fix it.

Fixes: 80fca30d5ba5 ("Add support multi-letter local variables")
Brown-paper-bag-by: Makoto Teramoto <mteramoto.knct@gmail.com>
Signed-off-by: Makoto Teramoto <mteramoto.knct@gmail.com>

codegen.c
@@ -43,18 +43,25 @@ static void store() {
   printf("    sd t1, 0(sp)\n");
 }
 
-static void prologue() {
+/**
+ * @brief Export prologue.
+ * @param stack_size The size of stack to push down for the local variables.
+ */
+static void prologue(int stack_size) {
   // Comply IALIGN=16 boundary.
   printf("    addi sp, sp, -16\n");
   printf("    sd fp, 0(sp)\n");
   printf("    addi fp, sp, 16\n");
-  printf("    addi sp, sp, -208\n");
+  printf("    addi sp, sp, -%d\n", stack_size);
 }
 
-static void epilogue() {
+/**
+ * @brief Export epilogue.
+ * @param stack_size The size of stack to push down for the local variables.
+ */
+static void epilogue(int stack_size) {
   printf(".Lreturn:\n");
-  // Release the region of local variables (208 bytes).
-  printf("    addi sp, fp, 208\n");
+  printf("    addi sp, fp, %d\n", stack_size);
   printf("    ld fp, 0(sp)\n");
   printf("    addi sp, sp, 16\n");
   printf("    ret\n");
@@ -149,7 +156,7 @@ void codegen(struct Program *prog) {
   printf(".globl main\n");
   printf("main:\n");
 
-  prologue();
+  prologue(prog->stack_size);
 
   // Emit the code
   for (struct Node *n = prog->node; n; n = n->next) {
@@ -158,5 +165,5 @@ void codegen(struct Program *prog) {
     printf("    addi sp, sp, 8\n");
   }
 
-  epilogue();
+  epilogue(prog->stack_size);
 }