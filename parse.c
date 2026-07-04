mtjoshishi/riscv_chibicc

commit d233448854410db0114fdb081e373add9277dd12

Add support single-letter local variables
Assisted-by: Gemini 3.5 Flash
Signed-off-by: Makoto Teramoto <mteramoto.knct@gmail.com>

parse.c
@@ -45,8 +45,20 @@ struct Node *new_num(int value) {
   return node;
 }
 
+/**
+ * @brief Create new local variable node.
+ * @param name The name of local variable.
+ * @return New 'NODE_LVAR' node.
+ */
+struct Node *new_lvar(char name) {
+  struct Node *node = new_node(NODE_LVAR);
+  node->name = name;
+  return node;
+}
+
 static struct Node *stmt(struct Token **token);
 static struct Node *expr(struct Token **token);
+static struct Node *assign(struct Token **token);
 static struct Node *equality(struct Token **token);
 static struct Node *relational(struct Token **token);
 static struct Node *add(struct Token **token);
@@ -86,13 +98,21 @@ static struct Node *stmt(struct Token **token) {
 }
 
 /**
- * @brief expr = equality
+ * @brief expr = assign
  * @param **token Tokenized source code.
- * @return Node for `equality`.
+ * @return Node for `expr`.
  */
 static struct Node *expr(struct Token **token) {
   CHECK(token != nullptr && *token != nullptr);
-  return equality(token);
+  return assign(token);
+}
+
+static struct Node *assign(struct Token **token) {
+  CHECK(token != nullptr && *token != nullptr);
+  struct Node *node = equality(token);
+  if (consume(token, "="))
+    node = new_binary(NODE_ASSIGN, node, assign(token));
+  return node;
 }
 
 /**
@@ -190,7 +210,7 @@ static struct Node *unary(struct Token **token) {
 }
 
 /**
- * @brief primary = num | "(" expr ")"
+ * @brief primary = num | ident | "(" expr ")"
  * @param **token Tokenized source code.
  * @return Either value node or node for `expr`.
  */
@@ -202,6 +222,10 @@ static struct Node *primary(struct Token **token) {
     seek_if_expect(token, ")");
     return node;
   }
+
+  struct Token *tok = consume_ident(token);
+  if (tok != nullptr)
+    return new_lvar(*tok->str);
 
   return new_num(seek_if_expect_number(token));
 }

test.sh
@@ -53,4 +53,6 @@ assert 0 "1 >= 2;"
 
 assert 3 "1; 2; 3;"
 
+assert 3 "a=3; a;"
+
 echo OK

chibicc_types.h
@@ -6,6 +6,7 @@
 // Kind of token
 enum TokenKind {
   TK_RESERVED, // Reserved word
+  TK_IDENT,    // Identifier
   TK_NUM,      // Numeric token
   TK_EOF,      // End of file
 };
@@ -23,15 +24,17 @@ struct Token {
 
 // Kind of node for AST
 enum NodeKind {
-  NODE_ADD, // '+'
-  NODE_SUB, // '-'
-  NODE_MUL, // '*'
-  NODE_DIV, // '/'
-  NODE_EQ,  // ==
-  NODE_NE,  // !=
-  NODE_LT,  // <
-  NODE_LE,  // <=
-  NODE_NUM, // number
+  NODE_ADD,    // '+'
+  NODE_SUB,    // '-'
+  NODE_MUL,    // '*'
+  NODE_DIV,    // '/'
+  NODE_EQ,     // ==
+  NODE_NE,     // !=
+  NODE_LT,     // <
+  NODE_LE,     // <=
+  NODE_ASSIGN, // =
+  NODE_LVAR,   // Local variable
+  NODE_NUM,    // number
 };
 
 // Node for AST
@@ -40,6 +43,7 @@ struct Node {
   struct Node *next;  // Next node.
   struct Node *lhs;   // Left hand side statement
   struct Node *rhs;   // Right hand side statement
+  char name;          // Name of variable. Use if kind is NODE_LVAR.
   int val;            // Value if kind is NODE_NUM
 };
 

codegen.c
@@ -5,6 +5,60 @@
 #include "chibicc_error.h"
 #include "chibicc_types.h"
 
+/// @brief Generate local variable into the stack.
+static void gen_addr(struct Node *node) {
+  CHECK(node != nullptr);
+  if (node->kind != NODE_LVAR)
+    error("Not an lvalue.");
+  int offset = (node->name - 'a' + 1) * 8;
+  printf("    addi t0, fp, -%d\n", offset);
+  printf("    addi sp, sp, -8\n");
+  printf("    sd t0, 0(sp)\n");
+}
+
+/// @brief Load the value from the stack.
+static void load() {
+  // Pop the address of the top, read the value, and assign to 't0'.
+  printf("    ld t0, 0(sp)\n");
+  printf("    addi sp, sp, 8\n");
+  // Pop the address of the top, read the value, and assign to 't1'.
+  printf("    ld t1, 0(t0)\n");
+  // Push the read value at 't1' to stack.
+  printf("    addi sp, sp, -8\n");
+  printf("    sd t1, 0(sp)\n");
+}
+
+/// @brief Store the data onto the stack.
+static void store() {
+  // Pop the rvalue from the top of stack.
+  printf("    ld t1, 0(sp)\n");
+  printf("    addi sp, sp, 8\n");
+  // Pop the lvalue from the top of stack.
+  printf("    ld t0, 0(sp)\n");
+  printf("    addi sp, sp, 8\n");
+  // Assign the value of 't1' into the address of 't0'
+  printf("    sd t1, 0(t0)\n");
+  // Push the value of 't1'
+  printf("    addi sp, sp, -8\n");
+  printf("    sd t1, 0(sp)\n");
+}
+
+static void prologue() {
+  // Comply IALIGN=16 boundary.
+  printf("    addi sp, sp, -16\n");
+  printf("    sd fp, 0(sp)\n");
+  printf("    addi fp, sp, 16\n");
+  printf("    addi sp, sp, -208\n");
+}
+
+static void epilogue() {
+  // Release the region of local variables (208 bytes).
+  printf("    addi sp, fp, 208\n");
+  printf("    ld fp, 0(sp)\n");
+  printf("    addi sp, sp, 16\n");
+  printf("    ret\n");
+}
+
 /**
  * @brief Generate assembly codes by traversing the AST.
  * @param node Parsed nodes of AST.
@@ -12,41 +66,53 @@
 static void gen(struct Node *node) {
   CHECK(node != nullptr);
 
-  if (node->kind == NODE_NUM) {
-    printf("    addi sp, sp, -8\n");
+  switch (node->kind) {
+  case NODE_NUM:
     printf("    li t0, %d\n", node->val);
-    printf("    sw t0, 0(sp)\n");
+    printf("    addi sp, sp, -8\n");
+    printf("    sd t0, 0(sp)\n");
     return;
+  case NODE_LVAR:
+    gen_addr(node);
+    load();
+    return;
+  case NODE_ASSIGN:
+    gen_addr(node->lhs);
+    gen(node->rhs);
+    store();
+    return;
+  default:
+    break;
   }
 
   gen(node->lhs);
   gen(node->rhs);
 
-  printf("    lw t1, 0(sp)\n");
+  printf("    ld t1, 0(sp)\n");
   printf("    addi sp, sp, 8\n");
-  printf("    lw t0, 0(sp)\n");
+  printf("    ld t0, 0(sp)\n");
   printf("    addi sp, sp, 8\n");
 
   switch (node->kind) {
   case NODE_ADD:
-    printf("    addw t0, t0, t1\n");
+    printf("    add t0, t0, t1\n");
     break;
   case NODE_SUB:
-    printf("    subw t0, t0, t1\n");
+    printf("    sub t0, t0, t1\n");
     break;
   case NODE_MUL:
-    printf("    mulw t0, t0, t1\n");
+    printf("    mul t0, t0, t1\n");
     break;
   case NODE_DIV:
-    printf("    divw t0, t0, t1\n");
+    printf("    div t0, t0, t1\n");
     break;
   case NODE_EQ:
-    printf("    subw t0, t0, t1\n");
+    printf("    sub t0, t0, t1\n");
     // Equals 'sltiu t0, t0, 1'. 'SLT' = Set less than
     printf("    seqz t0, t0\n");
     break;
   case NODE_NE:
-    printf("    subw t0, t0, t1\n");
+    printf("    sub t0, t0, t1\n");
     // Equals 'sltu t0, x0, t0'.
     printf("    snez t0, t0\n");
     break;
@@ -75,12 +141,14 @@ void codegen(struct Node *node) {
   printf(".globl main\n");
   printf("main:\n");
 
+  prologue();
+
   // Traverse the AST to emit assembly.
   for (struct Node *n = node; n; n = n->next) {
     gen(n);
-    printf("    lw a0, 0(sp)\n");
+    printf("    ld a0, 0(sp)\n");
     printf("    addi sp, sp, 8\n");
   }
 
-  printf("    ret\n");
+  epilogue();
 }

tokenize.c
@@ -48,6 +48,21 @@ bool consume(struct Token **token_ptr, char *op) {
 }
 
 /**
+ * @brief Consumes the current token if it is an identifier.
+ * @param token_ptr The pointer of a token to be consumed.
+ * @return The current token.
+ * @note The given pointer of token will be sought.
+ */
+struct Token *consume_ident(struct Token **token_ptr) {
+  CHECK(token_ptr != nullptr && *token_ptr != nullptr);
+  if ((*token_ptr)->kind != TK_IDENT)
+    return nullptr;
+  struct Token *current = *token_ptr;
+  *token_ptr = (*token_ptr)->next;
+  return current;
+}
+
+/**
  * @brief Seek the token if the expected character.
  * @param token_ptr The pointer of a token to seek.
  * @param op Expected string.
@@ -138,8 +153,16 @@ struct Token *tokenize(char *input) {
       continue;
     }
 
-    if (strchr("+-*/()<>;", *p)) {
+    if (strchr("+-*/()<>;=", *p)) {
       cur = new_token(TK_RESERVED, &cur, p, 1);
+      p++;
+      cur->source_input = input;
+      continue;
+    }
+
+    // Identifier
+    if ('a' <= *p && *p <= 'z') {
+      cur = new_token(TK_IDENT, &cur, p, 1);
       p++;
       cur->source_input = input;
       continue;

tokenize.h
@@ -6,6 +6,7 @@
 #include "chibicc_types.h"
 
 bool consume(struct Token **token_ptr, char *op);
+struct Token *consume_ident(struct Token **token_ptr);
 void seek_if_expect(struct Token **token_ptr, char *op);
 int seek_if_expect_number(struct Token **token_ptr);
 bool at_eof(struct Token **token_ptr);

commit 80a48c0cbe809f00670924397ccbca6b469c7b5e

parse: Fix searching of local variables
As mentioned in 127cb1c36e3b ("Add "for" statement (works for single
variable, but fails with multiples)"), the parser for "for" statements
fails when multiple variables are handled. I previously thought this
issue would be resolved by introducing the 'expression statement'
concept because I suspected a 'dirty stack' was the root cause.

However, that wasn't the case. It was simply because I was incompetent.

The parser searches the registered local variables by comparing their name
and length. For the latter, I mistakenly used 'strlen(var->name)' to get
the length, even though 'var->name' is NOT null-terminated. Thus, the
comparison 'strlen(var->name) == token->len' always failed due to the
undefined behavior of 'strlen()' on null-terminated string.

Fix this by using the 'len' member of the 'struct Var' instead.

Fixes: d23344885441 ("Add support single-letter local variables")
Fixes: 127cb1c36e3b ("Add "for" statement (works for single variable, but fails with multiples)")
Brown-paper-bag-by: Makoto Teramoto <mteramoto.knct@gmail.com>
Signed-off-by: Makoto Teramoto <mteramoto.knct@gmail.com>

parse.c
@@ -12,8 +12,7 @@ struct Var *locals;
 struct Var *find_var(struct Token *token) {
   CHECK(token != nullptr);
   for (struct Var *var = locals; var; var = var->next)
-    if (strlen(var->name) == token->len &&
-        !memcmp(token->str, var->name, var->len))
+    if (var->len == token->len && !memcmp(token->str, var->name, var->len))
       return var;
   return nullptr;
 }
@@ -88,6 +87,7 @@ struct Var *push_var(char *name) {
 
   var->next = locals;
   var->name = name;
+  var->len = strlen(name);
   locals = var;
   return var;
 }

test.sh
@@ -68,5 +68,6 @@ assert 10 "i=0; while(i<10) i=i+1; return i;"
 
 assert 10 "a=0; for (a = 0; a < 10; a = a + 1) a = a + 1; return a;"
 assert 3 "for (;;) return 3; return 5;"
+assert 55 "i=0; j=0; for (i=0; i<=10; i=i+1) j=i+j; return j;"
 
 echo OK