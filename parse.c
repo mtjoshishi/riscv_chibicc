724b67f369b7c6ff3ab19e34a46374338e91e54b
Makoto Teramoto <mteramoto.knct@gmail.com>
2026-08-03 07:18:07 +0900

    Add string literal initializer
    Signed-off-by: Makoto Teramoto <mteramoto.knct@gmail.com>

--- parse.c
+++ parse.c
@@ -906,11 +906,40 @@ static struct Node *lvar_init_zero(struct Token **token, struct Node *cur,
  *
  * If an initializer list is shorter than an array, excess array elements are
  * initialized with 0.
+ *
+ * A char array can be initialized by a string literal. For example,
+ * `char x[4] = "foo"` is equivalent to `char x[4] = {'f', 'o', 'o', '\0'}`.
  */
 static struct Node *lvar_initializer(struct Token **token, struct Node *cur,
                                      struct Var *var, struct Type *ty,
                                      struct Designator *desg) {
   CHECK(token != nullptr && *token != nullptr);
+  CHECK(ty != nullptr);
+
+  if (ty->kind == TYPE_ARRAY && ty->base->kind == TYPE_CHAR &&
+      (*token)->kind == TK_STR) {
+    // Initialize a char array with a string literal.
+    struct Token *tok = *token;
+    *token = (*token)->next;
+
+    long len =
+        (ty->array_size < tok->content_len) ? ty->array_size : tok->content_len;
+    int i;
+
+    for (i = 0; i < len; i++) {
+      struct Designator desg2 = {desg, i};
+      struct Node *rhs = new_num(tok->contents[i], tok);
+      cur->next = new_desg_node(var, &desg2, rhs);
+      cur = cur->next;
+    }
+
+    for (; i < ty->array_size; i++) {
+      struct Designator desg2 = {desg, i};
+      cur = lvar_init_zero(token, cur, var, ty->base, &desg2);
+    }
+    return cur;
+  }
+
   struct Token *tok = *token;
   if (!consume(token, "{")) {
     cur->next = new_desg_node(var, desg, assign(token));
